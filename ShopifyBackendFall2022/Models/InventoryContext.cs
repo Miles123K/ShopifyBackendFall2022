1f8366454e763fa35ac2a1fd6a3054ab33f84ae0
Miles123K <yh643@georgetown.edu>
2022-05-16 11:34:53 -0400

    Add project files.

---
+++ ShopifyBackendFall2022/Models/InventoryContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+
+namespace ShopifyBackendFall2022.Models
+{
+    public class InventoryContext : DbContext
+    {
+        public InventoryContext(DbContextOptions<InventoryContext> options) :
+            base(options)
+        { }
+
+
+        public DbSet<InventoryItem>? inventoryItems { get; set; }
+    }
+}
---
+++ ShopifyBackendFall2022/Models/InventoryItem.cs
@@ -0,0 +1,17 @@
+﻿namespace ShopifyBackendFall2022.Models
+{
+    public class InventoryItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string? Location { get; set; }
+
+        public string? Category { get; set; }
+        public InventoryItem(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
